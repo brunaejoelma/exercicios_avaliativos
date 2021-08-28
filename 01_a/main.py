7d720b75b8eac135435d8f6d2ae3946a0babd282
Bruna Carolina <notebruna@gmail.com>
2021-08-28 14:49:09 -0300

    Conclusão da tarefa 1a

--- 01_a/main.py
+++ 01_a/main.py
@@ -1,3 +1,3 @@
 from unidade_federativa import UnidadeFederativa
 print(UnidadeFederativa.get_lista_uf())
-
+print(UnidadeFederativa.get_uf(input('Digite uma UF ')))
--- 01_a/unidade_federativa.py
+++ 01_a/unidade_federativa.py
@@ -6,18 +6,84 @@ class UnidadeFederativa:
     def get_uf(sigla):
         if sigla == 'AC':
             return 'Acre'
+
         elif sigla == 'AL':
             return 'Alagoas'
+
         elif sigla == 'AP':
             return  'Amapá'
+
         elif sigla == 'AM':
             return 'Amazonas'
+
         elif sigla == 'BA':
             return 'Bahia'
+
         elif sigla == 'CE':
             return 'Ceará'
+
         elif sigla == 'DF':
             return 'Distrito Federal'
+
         elif sigla == 'ES':
             return 'Espírito Santo'
-        
+
+        elif sigla == "GO":
+            return "Goiás"
+
+        elif sigla == "MA":
+            return "Maranhão"
+
+        elif sigla == "MT":
+            return "Mato Grosso"
+
+        elif sigla == "MS":
+            return "Mato Grosso do Sul"
+
+        elif sigla == "MG":
+            return "Minas Gerais"
+
+        elif sigla == "PA":
+            return "Pará"
+
+        elif sigla == "PB":
+            return "Paraíba"
+
+        elif sigla == "PR":
+            return "Paraná"
+
+        elif sigla == "PE":
+            return "Pernambuco"
+
+        elif sigla == "PI":
+            return "Piauí"
+
+        elif sigla == "RJ":
+            return "Rio de Janeiro"
+
+        elif sigla == "RN":
+            return "Rio Grande do Norte"
+
+        elif sigla == "RS":
+            return "Rio Grande do Sul"
+
+        elif sigla == "RO":
+            return "Rondônia"
+
+        elif sigla == "RR":
+            return "Roraima"
+
+        elif sigla == "SC":
+            return "Santa Catarina"
+
+        elif sigla == "SP":
+            return "São Paulo"
+
+        elif sigla == "SE":
+            return "Sergipe"
+
+        elif sigla == "TO":
+            return "Tocantins"
+
+        else:
+            return 'UF inválida'
